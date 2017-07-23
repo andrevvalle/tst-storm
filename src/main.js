8f33355ff523a025ebc57ee566307eabfce39b48
André Valle <andrevvalle@icloud.com>
2017-07-23 20:00:42 -0300

    Install and config react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('ok')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App.jsx'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
